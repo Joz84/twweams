da2d02afecd61c82b725cae9d52ba890367de65b
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-10-16 00:27:05 +0200

    menu

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,13 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @users = User.where.not(latitude: nil, longitude: nil) #AJOUTER CRITERE DE BIRTHDAY
-
+    year = current_user.birthday.year
+    month = current_user.birthday.month
+    day = current_user.birthday.day
+    date_min = DateTime.new(year, month, day)
+    date_max = DateTime.new(year, month, day + 1)
+    all_users = User.where.not(latitude: nil, longitude: nil) #AJOUTER CRITERE DE BIRTHDAY
+    @users = all_users.select { |user| user.birthday >= date_min && user.birthday <= date_max }
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
