8f68ab24af8ec867bf453a34c2b98e594c9d6c57
Charly Martin <charly.martin.1207@gmail.com>
2016-10-15 15:09:32 +0200

    google maps working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
-      marker.infowindow render_to_string(partial: "/flats/map_box", locals: { user: user })
+      marker.infowindow render_to_string(partial: "/pages/map_box", locals: { user: user })
     end
   end
 
