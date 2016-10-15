4310574eae986ecbcd92839c3015c2830c5dc6b5
Charly Martin <charly.martin.1207@gmail.com>
2016-10-15 22:17:45 +0200

    new home / sign up / map

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,24 +7,22 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :address, presence: true
-  validates :zip_code, presence: true
   validates :city, presence: true
   validates :country, presence: true
   validates :birthday, presence: true
-  #
-  geocoded_by :full_address
-  after_validation :geocode, if: :full_address_changed?
+  geocoded_by :user_city
+  after_validation :geocode, if: :user_city_changed?
 
   def full_name
     "#{first_name} #{last_name}"
   end
 
-  def full_address
-    "#{address}, #{zip_code} #{city} #{ISO3166::Country[country].name}"
+  def user_city
+    "#{city} #{ISO3166::Country[country].name}"
   end
 
-  def full_address_changed?
-    address_changed? || zip_code_changed? || city_changed? || country_changed?
+  def user_city_changed?
+    city_changed? || country_changed?
+    # address_changed? || zip_code_changed? ||
   end
 end
