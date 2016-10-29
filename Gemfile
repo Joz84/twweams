84a1c4e3837131cd43007bacc2d2f9cf62c269a0
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-10-29 15:11:53 +0200

    home et formulaire

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-gem 'cloudinary'
+gem 'cloudinary', "1.1.0"
 gem 'attachinary', github: 'assembler/attachinary'
 gem "jquery-fileupload-rails"
 
--- db/migrate/20161015090117_add_columns_to_users.rb
+++ db/migrate/20161015090117_add_columns_to_users.rb
@@ -2,14 +2,19 @@ class AddColumnsToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :address, :string
-    add_column :users, :complement, :string
-    add_column :users, :zip_code, :string
+    #add_column :users, :address, :string
+    #add_column :users, :complement, :string
+    #add_column :users, :zip_code, :string
     add_column :users, :city, :string
     add_column :users, :country, :string
-    add_column :users, :charity, :boolean
-    add_column :users, :gift, :boolean
-    add_column :users, :travel, :boolean
+    add_column :users, :gender, :boolean
+    add_column :users, :bio, :text
+    add_column :users, :book, :string
+    add_column :users, :music, :string
+    add_column :users, :movie, :string
+    #add_column :users, :charity, :boolean
+    #add_column :users, :gift, :boolean
+    #add_column :users, :travel, :boolean
     add_column :users, :birthday, :datetime
     #add_reference :users, :birthday, index: true
   end
