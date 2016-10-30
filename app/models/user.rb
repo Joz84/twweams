b3a5efa29e9edd59210a2f9f7368c0e384041634
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-10-30 12:11:19 +0100

    debut nettoyage pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,11 @@ class User < ApplicationRecord
     city_changed? || country_changed?
     # address_changed? || zip_code_changed? ||
   end
+
+  def self.birthdate(params)
+    year = datetime_params["birthdate(1i)"].to_i
+    month = datetime_params["birthdate(2i)"].to_i
+    day = datetime_params["birthdate(3i)"].to_i
+    DateTime.new(year, month, day, 0, 0, 0, "+00:00")
+  end
 end
